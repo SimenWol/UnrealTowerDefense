17f70d8c212954a5939674226d4af35735f44a2b
SimenWol <simenwol@gmail.com>
2025-10-09 23:42:58 +0200

    add dependencies to TDCore
    - Still have to update rest of TDCore to work with newly added plugins

--- Source/UnrealTowerDefenseCore/UnrealTowerDefenseCore.Build.cs
+++ Source/UnrealTowerDefenseCore/UnrealTowerDefenseCore.Build.cs
@@ -5,6 +5,6 @@ public class UnrealTowerDefenseCore : ModuleRules
     public UnrealTowerDefenseCore(ReadOnlyTargetRules Target) : base(Target)
     {
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
-        PrivateDependencyModuleNames.AddRange(new string[] { "OmniCameraCore" });
+        PrivateDependencyModuleNames.AddRange(new string[] { "OmniCameraCore", "OmniWaveCore", "OmniWaveAssets", "TowerDefenseWaveCore", "TowerDefenseAICore" });
     }
 }
